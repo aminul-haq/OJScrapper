05c320b01d0c86f1c7895c839be9c7dfda399d62
Aminul Haq <roxaminul@gmail.com>
2020-08-29 01:05:45 +0600

    test ui added

--- app.py
+++ app.py
@@ -1,21 +1,57 @@
-from flask import Flask, Response, Request
+from flask import Flask, Response, url_for, render_template, send_from_directory
 from flask_pymongo import pymongo, PyMongo
 from scrappers.vjudge_profile_details_scrapper import *
 import db as database
 import json
+import jinja2.exceptions
 from flask import jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+#
+#
+# app.register_error_handler(404, page_not_found)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<path:path>')
+def send(path):
+    return render_template(str(path).replace(".html", "") + '.html')
 
 
 # app.config['MONGO_DBNAME'] = 'test'
 # app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@cluster0.s022h.mongodb.net/test?retryWrites=true&w=majority'
 # mongo = PyMongo(app)
 
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/<pagename>')
+# def admin(pagename):
+#     return render_template(pagename+'.html')
+#
+# @app.route('/<path:resource>')
+# def serveStaticResource(resource):
+# 	return send_from_directory('static/', resource)
+#
+#
+@app.errorhandler(jinja2.exceptions.TemplateNotFound)
+def template_not_found(e):
+    return not_found(e)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
 
 # test to insert data to the data base
@@ -43,7 +79,7 @@ def insert(car_price):
 def get_price(model):
     res = database.db.test.find({"name": model})
     res_list = [x for x in res]
-    return Response(json.dumps(res_list,default=str),mimetype="application/json")
+    return Response(json.dumps(res_list, default=str), mimetype="application/json")
 
 
 @app.route("/vjudge/<string:username>", methods=["GET"])
