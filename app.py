9a8d20df852edaa6819b135741b86781fccca33b
Aminul Haq <roxaminul@gmail.com>
2020-09-06 18:25:23 +0600

    added registration, login, jwt, lookup

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask, Response, Request
-from flask_pymongo import pymongo, PyMongo
-from scrappers.vjudge_profile_details_scrapper import *
-import db as database
-import json
-from flask import jsonify
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from resources.user import *
+from common.database import Database
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
+app.secret_key = "abcdxyz"
+
+api = Api(app)
+jwt = JWTManager(app)
 
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if UserModel.get_by_username(identity).is_admin:
+        return {'is_admin': True}
+    return {'is_admin': False}
 
 
-@app.route("/vjudge/<string:username>", methods=["GET"])
-def get_vjudge_profile(username):
-    try:
-        return solve_details(username), 200
-    except:
-        return "error", 404
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<str:username>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(Lookup, "/lookup")
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,13 +1,11 @@
 from flask_pymongo import pymongo
 
-
 # CONNECTION_STRING = "mongodb+srv://admin:admin@cluster0.s022h.mongodb.net/test?retryWrites=true&w=majority"
 # client = pymongo.MongoClient(CONNECTION_STRING)
 # db = client.get_database('test')
 # user_collection = pymongo.collection.Collection(db, 'test')
 
-client = pymongo.MongoClient(
-    "mongodb+srv://admin:admin@cluster0.s022h.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.s022h.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database("test")
 
 print(db.list_collection_names())
